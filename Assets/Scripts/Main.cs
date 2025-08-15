7efbe049ed2d66c03ba85aeab53d9b117b8f466a
xue-fei <awnuxcvbn@163.com>
2025-08-15 09:16:54 +0800

    ollamasharp-unity

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -6,6 +6,7 @@ public class Main : MonoBehaviour
 {
     public SpeechRecognition speechRecognition;
     MicrophoneWebGL microphoneWebGL;
+    OllamaSharpUnity ollama;
 
     // Start is called before the first frame update
     void Start()
@@ -17,9 +18,10 @@ public class Main : MonoBehaviour
             speechRecognition.onResult += OnResult;
             speechRecognition.onResultEnd += OnResultEnd;
         }
+        ollama = new OllamaSharpUnity("http://localhost:11434", "qwen2.5:1.5b", OnWord, OnSentence);
     }
 
-    void OnData(float[] data)
+    private void OnData(float[] data)
     {
         if (speechRecognition != null)
         {
@@ -27,13 +29,32 @@ public class Main : MonoBehaviour
         }
     }
 
-    public void OnResult(string result)
+    private void OnResult(string result)
     {
         Debug.Log(result);
     }
 
-    public void OnResultEnd(string result)
+    private void OnResultEnd(string result)
     {
         Debug.Log(result);
+        ollama.RequestAsync(result);
+    }
+
+    private void OnWord(string word)
+    {
+        Debug.Log($"{word}");
+    }
+
+    private void OnSentence(string sentence)
+    {
+        Debug.Log(sentence);
+    }
+
+    private void OnDestroy()
+    {
+        if (ollama != null)
+        {
+            ollama.Stop();
+        }
     }
 }
